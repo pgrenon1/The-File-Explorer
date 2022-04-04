dd1578f84c94b3c9d20adfdc1c78360cf644b17f
Phil <pgrenon.pg@gmail.com>
2022-04-03 21:18:27 -0400

    cleanup i guess

--- Game/Forest/Path/Path/Path/Fork/Right_Path/Cemetary/Mausoleum/Mausoleum_Door/Mausoleum_Entrance/Stairs/Mausoleum_Hall/Back_Wall/Passage/Tunnel/Tunnel/Fork/Right_Tunnel/Grate/Pond.bat
+++ Game/Forest/Path/Path/Path/Fork/Right_Path/Cemetary/Mausoleum/Mausoleum_Door/Mausoleum_Entrance/Stairs/Mausoleum_Hall/Back_Wall/Passage/Tunnel/Tunnel/Fork/Right_Tunnel/Grate/Pond.bat
@@ -1,45 +1,14 @@
-cls
-@ECHO off
-title Folder Locker
-
-:CHECK
-if EXIST %~n0 goto ENTRY
-if NOT EXIST %~n0 goto MDFOLDER
-
-:ENTRY
-attrib %~n0 | findstr "^....H" >nul && (
-  goto UNLOCK
-) || (
-  goto LOCK
-) 
-echo Unlock [U] or Lock [L]?
-set/p "un=>"
-if %un%==U goto UNLOCK
-if %un%==u goto UNLOCK
-if %un%==L goto LOCK
-if %un%==l goto LOCK
-echo Invalid choice.
-goto ENTRY
-
-:LOCK
-attrib +h %~n0
-echo Locked.
-goto End
-
-:UNLOCK
-echo Enter password to unlock %~n0.
-set/p "pass=>"
-if NOT %pass%==cattail goto FAIL
-attrib -h %~n0
-echo Unlocked successfully
-goto End
-
-:FAIL
-echo Invalid password
-goto end
-:MDFOLDER
-md %~n0
-echo %~n0 created.
-goto End
-:End
-echo end.
+cls 
+@ECHO off 
+title Pond 
+:UNLOCK 
+echo Enter password to unlock %~n0. 
+set/p "pass=>" 
+if NOT %pass%==cattail goto FAIL 
+echo Unlocked successfully 
+goto END 
+:FAIL 
+echo Invalid password 
+goto END 
+:END 
+echo end 
---
+++ Game/Lock.bat
@@ -0,0 +1,47 @@
+cls
+@echo off
+title Folder Locker
+set command=%1
+set pathToFolder=%2
+set folder=%3
+set password=%4
+set fullPath=%pathToFolder%\%folder%
+ECHO %fullPath%
+
+IF /I "%command%"=="u" GOTO UNLOCK
+IF /I "%command%"=="l" GOTO LOCK
+GOTO END
+
+:LOCK
+    ECHO Locking %fullPath%
+    attrib +h %fullPath%
+    ECHO locked %fullPath%
+
+        REM Creating unlocker batch file if provided password
+        IF [%password%] == [] GOTO END
+        @ECHO cls > %fullPath%.bat
+        @ECHO @ECHO off >> %fullPath%.bat
+        @ECHO title %folder% >> %fullPath%.bat
+        @ECHO :UNLOCK >> %fullPath%.bat
+        @ECHO echo Enter password to unlock %%~n0. >> %fullPath%.bat
+        @ECHO set/p "pass=>" >> %fullPath%.bat
+        @ECHO if NOT %%pass%%==%password% goto FAIL >> %fullPath%.bat
+        @ECHO attrib -h -s %fullPath%
+        @ECHO echo Unlocked successfully >> %fullPath%.bat
+        @ECHO goto END >> %fullPath%.bat
+        @ECHO :FAIL >> %fullPath%.bat
+        @ECHO echo Invalid password >> %fullPath%.bat
+        @ECHO goto END >> %fullPath%.bat
+        @ECHO :END >> %fullPath%.bat
+        @ECHO echo end >> %fullPath%.bat
+        GOTO END
+
+:UNLOCK
+    ECHO Unlocking %fullPath%
+    attrib -h %fullPath%
+    ECHO Unlocked %fullPath%
+    GOTO END
+
+:END
+    ECHO END
+    EXIT
